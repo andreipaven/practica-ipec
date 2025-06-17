4e5fa4da60ed7fc38b4e27edb8f1eaeffc4516ae
andreipaven <gigiijvan@gmail.com>
2025-06-17 15:12:59 +0300

    update frontend components and homePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Hol a</h1>} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );

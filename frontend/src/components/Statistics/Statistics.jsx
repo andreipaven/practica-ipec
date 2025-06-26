634992dc6696f4e84e5505b5667080fb81b7296a
Novac <nandrei98@yahoo.com>
2025-06-26 15:16:26 +0300

    update

--- frontend/src/components/Statistics/Statistics.jsx
+++ frontend/src/components/Statistics/Statistics.jsx
@@ -4,37 +4,61 @@ import themeColors from "../../Themes/themeColors.jsx";
 import CustomBox from "../Containers/CustomBox.jsx";
 import useResponsive from "../Hooks/useResponsive.jsx";
 import BoltIcon from "@mui/icons-material/Bolt";
+import ConstructionIcon from "@mui/icons-material/Construction";
+import EuroIcon from "@mui/icons-material/Euro";
 
 const Statistics = ({ totalConsumption }) => {
   const { isMediumScreen } = useResponsive();
-  const pricePerKwh = 0.25;
+  const pricePerKwh = 0.22;
 
   const stats = [
+    {
+      title: "Active Equipment",
+      icon: (
+        <ConstructionIcon
+          sx={{
+            backgroundColor: "rgb(234 88 12 / var(--tw-bg-opacity, 1))",
+            color: themeColors.palette.primary.light,
+            fontSize: "3em",
+            borderRadius: ".2em",
+            boxShadow: 4,
+          }}
+        />
+      ),
+      value: "128",
+      valueColor: "#2b6cb0",
+    },
+
     {
       title: "Total Consumption",
       icon: (
         <BoltIcon
           sx={{
             backgroundColor: themeColors.palette.secondary.main,
-            color: themeColors.palette.secondary.contrastText,
+            color: themeColors.palette.primary.light,
             fontSize: "3em",
-            boxShadow:
-              "rgb(204, 219, 232) 3px 3px 6px 2px inset, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px inset",
-
             borderRadius: ".2em",
+            boxShadow: 4,
           }}
         />
       ),
       value: `${totalConsumption.toFixed(0)} kWh`,
       valueColor: themeColors.palette.secondary.dark,
     },
-    {
-      title: "Active Equipment",
-      value: "128",
-      valueColor: "#2b6cb0",
-    },
+
     {
       title: "Estimated Cost",
+      icon: (
+        <EuroIcon
+          sx={{
+            backgroundColor: themeColors.palette.success.dark,
+            color: themeColors.palette.primary.light,
+            fontSize: "3em",
+            borderRadius: ".2em",
+            boxShadow: 4,
+          }}
+        />
+      ),
       value: `€${(totalConsumption * pricePerKwh).toFixed(0)}`,
       valueColor: "#2b6cb0",
     },
@@ -60,6 +84,7 @@ const Statistics = ({ totalConsumption }) => {
           padding="1.5em"
           borderRadius="1em"
           boxShadow="0 4px 10px rgba(3,0,0,0.4)"
+          alignItems="start"
         >
           <Typography textAlign="start" width={"100%"}>
             {stat.icon}

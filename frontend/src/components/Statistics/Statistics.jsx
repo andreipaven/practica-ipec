aff6ea7fb911c42476a344f4a7b1aedb8d38ce3a
Novac <nandrei98@yahoo.com>
2025-06-26 10:21:40 +0300

    update

--- frontend/src/components/Statistics/Statistics.jsx
+++ frontend/src/components/Statistics/Statistics.jsx
@@ -3,13 +3,28 @@ import { Typography } from "@mui/material";
 import themeColors from "../../Themes/themeColors.jsx";
 import CustomBox from "../Containers/CustomBox.jsx";
 import useResponsive from "../Hooks/useResponsive.jsx";
+import BoltIcon from "@mui/icons-material/Bolt";
 
 const Statistics = ({ totalConsumption }) => {
   const { isMediumScreen } = useResponsive();
+  const pricePerKwh = 0.25;
 
   const stats = [
     {
       title: "Total Consumption",
+      icon: (
+        <BoltIcon
+          sx={{
+            backgroundColor: themeColors.palette.secondary.main,
+            color: themeColors.palette.secondary.contrastText,
+            fontSize: "3em",
+            boxShadow:
+              "rgb(204, 219, 232) 3px 3px 6px 2px inset, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px inset",
+
+            borderRadius: ".2em",
+          }}
+        />
+      ),
       value: `${totalConsumption.toFixed(0)} kWh`,
       valueColor: themeColors.palette.secondary.dark,
     },
@@ -19,8 +34,8 @@ const Statistics = ({ totalConsumption }) => {
       valueColor: "#2b6cb0",
     },
     {
-      title: "Cost Estimat",
-      value: "€123.45",
+      title: "Estimated Cost",
+      value: `€${(totalConsumption * pricePerKwh).toFixed(0)}`,
       valueColor: "#2b6cb0",
     },
     {
@@ -46,6 +61,9 @@ const Statistics = ({ totalConsumption }) => {
           borderRadius="1em"
           boxShadow="0 4px 10px rgba(3,0,0,0.4)"
         >
+          <Typography textAlign="start" width={"100%"}>
+            {stat.icon}
+          </Typography>
           <Typography
             fontSize="1.1em"
             fontWeight="500"

7a811a3dc6a634d559f3ed81ec097ca5654f7cfd
andreipaven <gigiijvan@gmail.com>
2025-06-27 12:23:55 +0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
+import { ToastContainer } from "react-toastify/unstyled";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-      </Routes>
+      </Routes>{" "}
     </BrowserRouter>
   );
 }
